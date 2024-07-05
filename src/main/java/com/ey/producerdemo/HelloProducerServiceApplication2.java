8375c8925701297079e263e79b04b0d7929734fc
rashmibondili <bondilirashmi2408@gmail.com>
2024-07-05 12:35:08 +0530

    first commit

---
+++ src/main/java/com/ey/producerdemo/HelloProducerServiceApplication2.java
@@ -0,0 +1,15 @@
+package com.ey.producerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class HelloProducerServiceApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloProducerServiceApplication2.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/producerdemo/ProducerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.producerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
